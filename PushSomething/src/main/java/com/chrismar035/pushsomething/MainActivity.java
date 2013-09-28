ad37e8b0bfabb68ff2cba5dd27cb7c0aa2ae60f0
Chris Marshall <chris@chrismar035.com>
2013-09-28 10:14:41 -0400

    Use correct sender ID to register for GCM

--- PushSomething/src/main/java/com/chrismar035/pushsomething/MainActivity.java
+++ PushSomething/src/main/java/com/chrismar035/pushsomething/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
     private static final String PROPERTY_APP_VERSION = "appVersion";
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
-    String SENDER_ID = "AIzaSyBAVyD-7VaZiDmhP0mdTV4RjhJm90eWun0";
+    String SENDER_ID = "762651962812";
 
     static final String TAG = "PushSomething";
 
