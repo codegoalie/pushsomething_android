19aa234a27e3d3b2afe835dc2a80adf09947d388
Chris Marshall <chris@chrismar035.com>
2013-10-04 08:50:32 -0400

    Allow users to sign in with G+

--- PushSomething/src/main/java/com/chrismar035/pushsomething/MainActivity.java
+++ PushSomething/src/main/java/com/chrismar035/pushsomething/MainActivity.java
@@ -2,8 +2,10 @@ package com.chrismar035.pushsomething;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -13,19 +15,30 @@ import android.app.Activity;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.internal.c;
+import com.google.android.gms.plus.PlusClient;
 
 import java.io.IOException;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+        View.OnClickListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
     public static final String PROPERTY_REG_ID = "registration_id";
     private static final String PROPERTY_APP_VERSION = "appVersion";
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    private ProgressDialog mConnectionProgressDialog;
+    private PlusClient mPlusClient;
+    private ConnectionResult mConnectionResult;
 
     String SENDER_ID = "762651962812";
 
@@ -41,8 +54,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlusClient = new PlusClient.Builder(this, this, this)
+                .setVisibleActivities("http://schemas.google.com/AddActivity")
+                .build();
+        mConnectionProgressDialog = new ProgressDialog(this);
+        mConnectionProgressDialog.setMessage("Signing in...");
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
         mDisplay = (TextView) findViewById(R.id.main_view);
-        mDisplay.append("\n" + SENDER_ID + "\n");
 
         context = getApplicationContext();
 
@@ -55,11 +75,40 @@ public class MainActivity extends Activity {
                 new backgroundRegistrationTask().execute();
             }
             Log.i(TAG, "Registration successful. Reg ID: " + regID);
-            mDisplay.append("\n" + regID + "\n");
-
         } else {
             mDisplay.setText("Google Play app not installed");
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i(TAG, "Handling sign in");
+
+        if (v.getId() == R.id.sign_in_button && !mPlusClient.isConnected()) {
+            if (mConnectionResult == null) {
+                mConnectionProgressDialog.show();
+                Log.i(TAG, "showing progress");
+            } else {
+                try {
+                    mConnectionResult.startResolutionForResult(this, PLAY_SERVICES_RESOLUTION_REQUEST);
+                } catch (IntentSender.SendIntentException e) {
+                    mConnectionResult = null;
+                    mPlusClient.connect();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPlusClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPlusClient.disconnect();
     }
 
     private class backgroundRegistrationTask extends AsyncTask<Void, Void, String> {
@@ -163,6 +212,47 @@ public class MainActivity extends Activity {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mConnectionProgressDialog.isShowing()) {
+            Log.i(TAG, "Connection failed");
+            // The user clicked sign in already. Start to resolve connection errors.
+            if (result.hasResolution()) {
+                try {
+                    result.startResolutionForResult(this, PLAY_SERVICES_RESOLUTION_REQUEST);
+                } catch (IntentSender.SendIntentException e) {
+                    mPlusClient.connect();
+                }
+            }
+        }
+
+        // Save the intent to start an activity when the user clicks the sign in
+        mConnectionResult = result;
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // We've resolved any errors
+        mConnectionProgressDialog.dismiss();
+        String accountName = mPlusClient.getAccountName();
+        String msg =  accountName + " is connected.";
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        Log.i(TAG, msg);
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.d(TAG, "disconnected");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if (requestCode == PLAY_SERVICES_RESOLUTION_REQUEST && responseCode == RESULT_OK) {
+            mConnectionResult = null;
+            mPlusClient.connect();
+        }
     }
 
 }
