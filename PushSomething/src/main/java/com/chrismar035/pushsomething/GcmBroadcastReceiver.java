3209b90085463a2f60ce0026415f2dad23f582d1
Chris Marshall <chris@chrismar035.com>
2013-10-27 11:20:47 -0400

    Notifications from the server are persisted

--- PushSomething/src/main/java/com/chrismar035/pushsomething/GcmBroadcastReceiver.java
+++ PushSomething/src/main/java/com/chrismar035/pushsomething/GcmBroadcastReceiver.java
@@ -12,10 +12,14 @@ import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import java.sql.SQLException;
+
 public class GcmBroadcastReceiver extends BroadcastReceiver {
     private static Context mContext;
     public static final int NOTIFICATION_ID = 1;
     static final String TAG = "PushSomething";
+
+    private NotificationsDataSource dataSource;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -33,7 +37,8 @@ public class GcmBroadcastReceiver extends BroadcastReceiver {
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
                 sendStringNotification("Deleted messages on server", extras.toString());
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                sendExtrasNotification(extras);
+                Notification notification = notificationFromBundle(extras);
+                sendNotification(notification);
             }
         } else {
             Log.i(TAG, "Extras is empty. No notification sent.");
@@ -59,9 +64,28 @@ public class GcmBroadcastReceiver extends BroadcastReceiver {
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 
-    private void sendExtrasNotification(Bundle extras) {
-        Log.i(TAG, "Sending Extras Notification");
+    private void sendNotification(Notification notification) {
+        Log.i(TAG, "Sending Notification");
 
+        NotificationManager mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, new Intent(mContext, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mContext)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(notification.getTitle());
+
+        if(!notification.getBody().isEmpty()) {
+            mBuilder.setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(notification.getBody()))
+                .setContentText(notification.getBody());
+        }
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+    private Notification notificationFromBundle(Bundle extras) {
         String title, body;
 
         if(extras.containsKey("title")) {
@@ -74,21 +98,19 @@ public class GcmBroadcastReceiver extends BroadcastReceiver {
         } else {
             body = "";
         }
-        NotificationManager mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        dataSource = new NotificationsDataSource(mContext);
+        try {
+            dataSource.open();
+            Notification notification = dataSource.createNotification(title, body);
 
-        PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, new Intent(mContext, MainActivity.class), 0);
+            Intent intent = new Intent();
+            intent.setAction("com.chrismar035.pushsomething.UpdateNotificationList");
+            mContext.sendBroadcast(intent);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mContext)
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle(title);
-
-        if(!body.isEmpty()) {
-            mBuilder.setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText(body))
-                .setContentText(body);
+            return notification;
+        } catch (SQLException e) {
+            Log.e(TAG, "Unable to open Notification data source in GCM receiver notification from bundle");
+            return new Notification(title, body);
         }
-
-        mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 }
