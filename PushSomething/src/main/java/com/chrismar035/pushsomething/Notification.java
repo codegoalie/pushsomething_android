e09b5717d46f9d1d6af2c8af4d23798f679efc48
Chris Marshall <chris@chrismar035.com>
2013-12-25 22:38:56 -0500

    Cancel acknowledged notifications

--- PushSomething/src/main/java/com/chrismar035/pushsomething/Notification.java
+++ PushSomething/src/main/java/com/chrismar035/pushsomething/Notification.java
@@ -4,13 +4,16 @@ package com.chrismar035.pushsomething;
  * Created by chrismar035 on 10/26/13.
  */
 public class Notification {
-    private long id;
+    private int id;
+    private int server_id;
     private String title;
     private String body;
     private long createdAt;
 
     public long getId() { return id; }
-    public void setId(long id) { this.id = id; }
+    public void setId(int id) { this.id = id; }
+    public long getServerId() { return server_id; }
+    public void setServerId(int server_id) { this.server_id = server_id; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
     public String getBody() { return body; }
@@ -20,7 +23,9 @@ public class Notification {
 
     public Notification() {}
 
-    public Notification(String title, String body) {
+    public Notification(int id, int server_id, String title, String body) {
+        this.id = id;
+        this.server_id = server_id;
         this.title = title;
         this.body = body;
         this.createdAt = System.currentTimeMillis()/1000;
@@ -30,6 +35,4 @@ public class Notification {
     public String toString() {
         return title;
     }
-
-
 }
